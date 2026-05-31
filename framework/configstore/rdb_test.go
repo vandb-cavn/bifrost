1fc794a02832c41efd76fa5bd2d528a3b415da88
VD <van@git>
2026-05-31 15:51:23 +0700

    test(configstore): add missing table migrations to setupRDBTestStore to fix publishing config store tests

--- framework/configstore/rdb_test.go
+++ framework/configstore/rdb_test.go
@@ -44,6 +44,9 @@ func setupRDBTestStore(t *testing.T) *RDBConfigStore {
 		&tables.TablePromptSessionMessage{},
 		&tables.TableOauthUserSession{},
 		&tables.TableOauthUserToken{},
+		&tables.TableGovernanceConfig{},
+		&tables.TableFrameworkConfig{},
+		&tables.TablePricingOverride{},
 	)
 	require.NoError(t, err, "Failed to migrate test database")
 
